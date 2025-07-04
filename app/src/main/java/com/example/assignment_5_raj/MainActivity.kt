01123ec44efb5b83bda067d6bacc4ea404038a90
“Raj <W0836062@myscc.ca>
2025-07-03 23:30:43 -0400

    completed assignment 6

--- app/src/main/java/com/example/assignment_5_raj/MainActivity.kt
+++ app/src/main/java/com/example/assignment_5_raj/MainActivity.kt
@@ -55,7 +55,6 @@ class MainActivity : AppCompatActivity() {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
         })
-
         removeBtn.setOnClickListener {
             if (listOfExpence.isNotEmpty()) {
                 listOfExpence.removeAt(listOfExpence.size - 1)
